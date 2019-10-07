8001da3c38570043f4c407dea9da3b59d39d196c
Leonheart <juboutbaiba@gmail.com>
2019-10-07 15:26:23 +0200

    link menu

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -27,4 +27,54 @@ class HomeController extends AbstractController
     {
         return $this->render('home/Lachatterie.html.twig');
     }
+
+    /**
+     * @Route("/race", name="race")
+     */
+
+    public function race()
+    {
+        return $this->render('home/larace.html.twig');
+    }
+
+    /**
+     * @Route("/males", name="males")
+     */
+    public function males()
+    {
+        return $this->render('home/males.html.twig');
+    }
+
+    /**
+     * @Route("/femelles", name="femelles")
+     */
+    public function femelles()
+    {
+        return $this->render('home/femelles.html.twig');
+    }
+
+    /**
+     * @Route("/chatons", name="chatons")
+     */
+    public function chatons()
+    {
+        return $this->render('home/chatons.html.twig');
+    }
+
+    /**
+     * @Route("/retraites", name="retraites")
+     */
+    public function retraites()
+    {
+        return $this->render('home/retraites.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('home/contact.html.twig');
+    }
+
 }
